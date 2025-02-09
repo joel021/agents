81f3b92d98a09b95a26dc2024694b94a9fb90448
jubsribs <julianacrispina@gmail.com>
2025-02-09 16:19:29 -0300

    feat:criando arquivo teste do agente research

---
+++ tests/agents/core/research_agent/test_research.py
@@ -0,0 +1,33 @@
+import os
+import unittest
+
+from agents.config import WORK_DIR
+from agents.constants import PROJECT_MANAGER_AGENT_NAME, RESEARCH_AGENT_NAME
+from agents.core.llm_reasoner import get_new_llm_reasoner
+from agents.core.message import Message
+from agents.core.research_agent.research_agent import ResearchAgent
+from agents.utils.redis_utils import get_redis_conn
+
+
+class TestResearchAgent(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        os.makedirs(f"{WORK_DIR}/test", exist_ok=True)
+
+    def test_reason_search_query(self):
+
+        redis_instance, pubsub = get_redis_conn()
+        research_agent = ResearchAgent(get_new_llm_reasoner(), redis_instance)
+
+        message = Message(
+            sender=PROJECT_MANAGER_AGENT_NAME,
+            recipient=RESEARCH_AGENT_NAME,
+            message=f"Create or replace a file, at {WORK_DIR}/test/new_file.py with the following contents:"
+                    f"print('Hello World!')",
+            data=None
+        )
+        performed = research_agent.reason(message.to_dict())
+
+        assert performed, "Performed without errors."
+
